eeeb50d134b1ecee0233e3615e33b382dc5dd0d5
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2019-02-03 15:02:46 +0100

    initial fill have working base tests

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name unitTestExploreApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the unitTestExploreApp
+ */
+angular.module('unitTestExploreApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name unitTestExploreApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the unitTestExploreApp
+ */
+angular.module('unitTestExploreApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
