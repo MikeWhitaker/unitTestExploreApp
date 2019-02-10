a33c79d807faa16f6909e65f21a33393b534c430
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2019-02-10 21:24:01 +0100

    before the part on resolving promises

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -8,10 +8,11 @@
  * Controller of the unitTestExploreApp
  */
 angular.module('unitTestExploreApp')
-  .controller('SearchCtrl', function ($scope, $location) {
-    $scope.search = function() {
-      if ($scope.query) {
-        $location.path('/results').search('q', $scope.query);
+  .controller('SearchCtrl', function ($location) {
+    var vm = this;
+    vm.search = function() {
+      if (vm.query) {
+        $location.path('/results').search('q', vm.query);
       }
     };
   });
--- test/spec/controllers/search.js
+++ test/spec/controllers/search.js
@@ -1,46 +1,34 @@
 "use strict";
 
-fdescribe("Search controller ->", function() {
-  var $scope;
+describe("Search controller ->", function() {
+  var vm;
   var $location;
   var $controller;
-
+  beforeEach(module('unitTestExploreApp'));
+  
   beforeEach(inject(function(_$controller_, _$location_) {
-    $scope = {};
     $controller = _$controller_;
     $location = _$location_;
-
-    var ctrlCtor = function($scope) {
-      
-      $scope.search = function() {
-        if ($scope.query) {
-          $location.path('/results').search('q', $scope.query);
-        }
-      };
-    };
-
-    var ctrlLocals = {
-      $scope: $scope,
-      $location: $location
-    };
-
-    $controller(ctrlCtor, ctrlLocals);
+    vm = {};
   }));
 
    it("should redirect to the query results page for non-empty query", function() {
     // Arrange
-    $scope.query = "star wars";
+    vm = $controller('SearchCtrl', {$location: $location}, {query: 'star wars'});
+
     // Act
-    $scope.search();
+    vm.search();
 
     // Assert
     expect($location.url()).toBe("/results?q=star%20wars");
   });
 
   it("should not do anything if the query is empty", function() {
-    $scope.query = "";
+    // Arrange
+    vm = $controller('SearchCtrl', {$location: $location}, {query: ''});
+    
     // Act
-    $scope.search();
+    vm.search();
 
     // Assert
     expect($location.url()).toBe("");
