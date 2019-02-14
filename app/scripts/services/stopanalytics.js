cd27ca928b0c52c30d0bdf591b65ac6dd021ed7b
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2019-02-14 21:29:35 +0100

    wrotesome wrapper for google analytics

--- app/scripts/services/stopanalytics.js
+++ app/scripts/services/stopanalytics.js
@@ -9,9 +9,8 @@
  */
 angular.module('unitTestExploreApp')
   .service('stopAnalytics', function (Analytics) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-
     var service = {};
+    
     service.trackEvent = function () {
       Analytics.trackEvent(arguments[0],[1],[3]);
     };
