6c757a1511776c57ba0a4f6581a5003cac1b87c6
Mike Whitaker <whitaker.mj@gmail.com>
2022-09-24 20:46:08 +0200

    Added tests for duration of years and months.

--- app/scripts/services/myservice.js
+++ app/scripts/services/myservice.js
@@ -15,5 +15,36 @@ angular.module('unitTestExploreApp')
       console.log('this is the locale: ', locale);
     };
 
+
+    service.getDurationInMonths = function (startDate, endDate) {
+      var startDateTime = new Date(startDate);
+      var endDateTime = new Date(endDate);
+
+      var months = (endDateTime.getFullYear() - startDateTime.getFullYear()) * 12;
+      months -= startDateTime.getMonth();
+      months += endDateTime.getMonth();
+
+      return months <= 0 ? 0 : months;
+    };
+
+    service.getDurationInYears = function (startDate, endDate) {
+      var startDateTime = new Date(startDate);
+      var endDateTime = new Date(endDate);
+
+      var years = (endDateTime.getFullYear() - startDateTime.getFullYear());
+
+      return years <= 0 ? 0 : years;
+    }
+
+    service.getDurationInYearsWithMoment = function (startDate, endDate) {
+      var startMoment = moment(new Date(startDate));
+      var endMoment = moment(new Date(endDate));
+
+      var durationInYears = endMoment.diff(startMoment, 'years');
+
+      console.log('durationInYears: ', durationInYears);
+      return durationInYears;
+    };
+    
     return service;
   });
--- test/spec/services/myservice.js
+++ test/spec/services/myservice.js
@@ -15,4 +15,59 @@ describe('Service: myService', function () {
     expect(!!myService).toBe(true);
   });
 
+  describe('getDurationInMonths', function () {
+    it('should return 0 if the start date is after the end date', function () {
+      var duration = myService.getDurationInMonths('2014-01-01', '2013-01-01');
+      expect(duration).toBe(0);
+    });
+
+    it('should return one when the for the difference between 2022-03-03 and 2022-04-04', function () {
+      var duration = myService.getDurationInMonths('2022-03-03', '2022-04-04');
+      expect(duration).toBe(1);
+    });
+  });
+
+  describe('getDurationInYears', function () {
+    it('should return 0 if the start date is after the end date', function () {
+      var duration = myService.getDurationInYears('2014-01-01', '2013-01-01');
+      expect(duration).toBe(0);
+    });
+
+    it('should return one when the for the difference between 2022-03-03 and 2023-04-04', function () {
+      var duration = myService.getDurationInYears('2022-03-03', '2023-04-04');
+      expect(duration).toBe(1);
+    });
+
+    it('should return one when the for the difference between 2022-03-03 and 2023-04-04', function () {
+      var duration = myService.getDurationInYears('2022-11-03', '2023-01-04');
+      expect(duration).toBe(1);
+    });
+
+    it('should return 10 when the for the difference between 2022-01-01 and 2032-10-01', function () {
+      var duration = myService.getDurationInYears('2022-07-01', '2032-05-01');
+      expect(duration).toBe(10);
+    });
+  });
+
+  describe('getDurationInYearsWithMoment', function () {
+    it('should return one when the for the difference between 2022-03-03 and 2023-04-04', function () {
+      var duration = myService.getDurationInYearsWithMoment('2022-03-03', '2023-04-04');
+      expect(duration).toBe(1);
+    });
+
+    it('should return one when the for the difference between 2022-03-03 and 2023-04-04', function () {
+      var duration = myService.getDurationInYearsWithMoment('2022-11-03', '2023-01-04');
+      expect(duration).toBe(0);
+    });
+
+    it('should return one when the for the difference between 2022-03-03 and 2023-04-04', function () {
+      var duration = myService.getDurationInYearsWithMoment('2022-04-03', '2023-04-01');
+      expect(duration).toBe(0);
+    });
+
+    it('should return 10 when the for the difference between 2022-01-01 and 2032-01-01', function () {
+      var duration = myService.getDurationInYearsWithMoment('2022-01-01', '2032-01-01');
+      expect(duration).toBe(10);
+    });
+  });
 });
